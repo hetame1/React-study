b2bc1adaa85ef6cfbd082f8c31ec78c29ec80898
hetame1 <poi8946@naver.com>
2023-09-22 15:32:01 +0900

    react-query 공부

---
+++ react-query/Blog-em/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import { Posts } from './Posts'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
+  )
+}
+
+export default App
