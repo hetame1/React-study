f966f8a3bdb8edafaab9c93393fac4f37862b1fa
hetame1 <poi8946@naver.com>
2023-12-18 20:25:02 +0900

    12/18 공부 : \n 들어가있는 데이터 띄어쓰기 위한 Text 컴포넌트

---
+++ react-wedding/src/components/shared/Text.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Text = ({ children }: { children: string }) => {
+  const message = children.split('\n').map((str, idx, array) => {
+    return (
+      <React.Fragment>
+        {str}
+        {idx === array.length - 1 ? null : <br />}
+      </React.Fragment>
+    )
+  })
+
+  return <div>{message}</div>
+}
+
+export default Text
