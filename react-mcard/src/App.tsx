aa590e7ed3d71aff82df68d33644f24bed013aa5
hetame1 <poi8946@naver.com>
2025-02-04 17:35:37 +0900

    공통 컴포넌트 구현
    Alert, Input, TextField, Flex

--- react-mcard/src/App.tsx
+++ react-mcard/src/App.tsx
@@ -1,7 +1,13 @@
+import Alert from './components/shared/Alert'
 import Button from './components/shared/Button'
+import Input from './components/shared/Input'
 import Text from './components/shared/Text'
+import TextField from './components/shared/TextField'
+import { useAlertContext } from './contexts/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
+
   return (
     <div>
       <Text typography="t1" color="black" bold>
@@ -10,6 +16,33 @@ function App() {
 
       <div style={{ height: 10, width: '100%', backgroundColor: 'red' }}></div>
       <Button color="primary" size="large">
+        버튼
+      </Button>
+
+      <Input placeholder="입력하세요" aria-invalid="false" />
+
+      <TextField label="입력하세요" aria-invalid="false" />
+
+      {/* <Alert
+        title="알림"
+        description="알림 안내를 보여줍니다."
+        buttonLabel="확인"
+        onButtonClick={() => console.log('clicked')}
+        open={true}
+      /> */}
+
+      <Button
+        color="primary"
+        size="large"
+        onClick={() =>
+          open({
+            title: '알림',
+            description: '알림 안내를 보여줍니다.',
+            buttonLabel: '확인',
+            onButtonClick: () => console.log('clicked'),
+          })
+        }
+      >
         버튼
       </Button>
     </div>
