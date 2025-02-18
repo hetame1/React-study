567f6d7a6968bad786ec7d289212c7baca95dd6b
hetame1 <poi8946@naver.com>
2025-02-19 00:28:17 +0900

    ch2/ 9, 10, 11, 12
    컬러 토큰 완성, 다크모드 생성, 타이포그라피 토큰 생성, 박스 토큰 생성

--- react-design-system/packages/themes/scripts/build-css-module.js
+++ react-design-system/packages/themes/scripts/build-css-module.js
@@ -1,6 +1,11 @@
 import * as theme from "../dist/index.js";
 import fs from "fs";
 
+// theme.css
+// :root {
+//   --gray-900: #171923
+// }
+
 const toCssCasting = (str) => {
   return str
     .replace(/([a-z])(\d)/, "$1-$2")
@@ -32,8 +37,80 @@ const generateThemeCssVariables = () => {
 
           return cssString.push(`${selector} {\n${cssVariables}\n}`);
         }
+
+        if (colorKey === "dark") {
+          const selector = ":root .theme-dark";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )}: ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+
+          return cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
       });
+
+      return;
     }
+
+    const selector = ":root";
+
+    const cssVariables = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(
+            ([subKey, subValue]) =>
+              `--${toCssCasting(mainKey)}-${toCssCasting(subKey)}: ${subValue};`
+          )
+          .join("\n")
+      )
+      .join("\n");
+
+    return cssString.push(`${selector} {\n${cssVariables}\n}`);
+  });
+  return cssString;
+};
+
+// .headingxl {
+//   font-size: 3rem;
+//   font-weight: 700;
+//   line-height: 100%;
+// }
+
+const generateThemeCssClasses = () => {
+  const cssString = [];
+
+  Object.entries(theme.classes).forEach(([, value]) => {
+    const cssClasses = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(([subKey, subValue]) => {
+            const className = `.${toCssCasting(mainKey)}${toCssCasting(
+              subKey
+            )}`;
+
+            const styleProperties = Object.entries(subValue)
+              .map(
+                ([styleKey, styleValue]) =>
+                  `${toCssCasting(styleKey)}: ${styleValue};`
+              )
+              .join("\n");
+
+            return `${className} {\n${styleProperties}\n}`;
+          })
+          .join("\n")
+      )
+      .join("\n");
+
+    cssString.push(cssClasses);
   });
 
   return cssString;
@@ -41,8 +118,9 @@ const generateThemeCssVariables = () => {
 
 const generateThemeCss = () => {
   const variables = generateThemeCssVariables();
+  const classes = generateThemeCssClasses();
 
-  fs.writeFileSync("dist/theme.css", [...variables].join("\n"));
+  fs.writeFileSync("dist/themes.css", [...variables, ...classes].join("\n"));
 };
 
 generateThemeCss();
---
+++ react-design-system/services/test/src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "@emotion/styled";
+import { vars, classes } from "@design/themes";
+
+function App() {
+  return <View />;
+}
+
+export default App;
+
+const View = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Text>Hello</Text>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const Text = styled.p`
+  ${classes.typography.heading["4xl"]}
+  color: ${vars.colors.$static.light.red[900]};
+`;
