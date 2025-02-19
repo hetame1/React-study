361da066a21631a40745d509d89f38a07194014e
hetame1 <poi8946@naver.com>
2025-02-19 15:53:37 +0900

    ch3 / 1,2,3,4
    모노레포 nx 설정, 공통 config 설정

--- react-design-system/packages/themes/build.js
+++ react-design-system/packages/themes/build.js
@@ -1,46 +1,9 @@
-import esbuild from "esbuild";
 import { readFileSync } from "fs";
-import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import run from "@design/esbuild-config";
 
-const require = createRequire(import.meta.url);
-const pkg = JSON.parse(readFileSync("./package.json"));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(readFileSync(join(__dirname, "package.json"), "utf8"));
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.devDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  format: "esm",
-  target: "es2019",
-  watch,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  esbuild
-    .build({
-      ...baseConfig,
-      format: "cjs",
-      outExtension: {
-        ".js": ".cjs",
-      },
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    }),
-]);
+run({ pkg });
